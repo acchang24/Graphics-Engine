4673e2df0e15a6a8c67459cb89e53a5b7ce73119
acchang24 <acchang24@gmail.com>
2023-02-28 11:38:57 -0800

    Add gl math library

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "Shader.h"
 #include "Texture.h"
 #include "VertexBuffer.h"
